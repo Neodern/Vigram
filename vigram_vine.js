1547f6558a7d75dd419af3541cf9d2f4a7cca031
Neodern <neodern@gmail.com>
2016-06-03 10:48:02 +0200

    new version of Vine downloader but not working ...

--- vigram_vine.js
+++ vigram_vine.js
@@ -1,142 +1,58 @@
 /**
  * Vigram
- * @version : 2.0.1
+ * @version : 3.0.0
  * @author: Nicolas (@neodern) Jamet <neodern@gmail.com>
  * @about: Download pics & videos from Vine & Instagram with a single click !
  */
 
 var image = chrome.extension.getURL("medias/images/vigram_128.png");
 
-/**
- * AJAX call.
- * @param verb
- * @param url
- * @param cb
- */
-function    ajax(verb, url, cb, index)
-{
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function(url) {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200)
-            return cb(xmlhttp.responseText, index);
-    };
-    if (url === null)
-        url = '';
-    xmlhttp.open(verb, url, true);
-    xmlhttp.send();
-}
-
-/**
- * Vine - Single page. <o/
- */
-function singleVine()
-{
-    var badges = document.querySelectorAll('.badges')[0];
-    if (typeof badges !== 'undefined' && document.querySelectorAll('#VineButton').length == 0)
-    {
-        var vineContainer = document.querySelectorAll('.vine-video-container')[0];
-        var fullUrl = vineContainer.querySelector('video').getAttribute('src');
-        if (typeof fullUrl !== 'undefined')
-        {
-            var url = fullUrl.split('?')[0];
-            var tmp = url.split('/');
-            tmp = tmp[tmp.length - 1];
-            var name = tmp.substring(0, tmp.indexOf('.')),
-                VigramBlock = document.createElement('div'),
-                VigramLink = document.createElement('a'),
-                VigramButton = document.createElement('img'),
-                VigramText = document.createElement('p');
-
-            VigramBlock.className = 'share-badge';
-            VigramLink.id = 'VineButton';
-            VigramLink.href = url;
-            VigramLink.setAttribute('download', name);
-            VigramButton.src = image;
-            VigramText.innerHTML = 'Vigram';
-
-            VigramLink.appendChild(VigramButton);
-            VigramLink.appendChild(VigramText);
-            VigramBlock.appendChild(VigramLink);
-            badges.insertBefore(VigramBlock, badges.querySelector('.twitter'));
-        }
-    }
-}
-
-function getChannelsVigramButton(container, url, name)
-{
-    var VigramLink = document.createElement('a'),
-        VigramButton = document.createElement('img'),
-        VigramText = document.createElement('span');
-
-    container.className += " VineButtonChannels";
-    container.id = '';
-    VigramText.className = 'caption';
-    VigramText.innerText = 'Vigram';
-    VigramLink.className = 'VineButtonChannels';
-    VigramLink.href = url;
-    VigramLink.setAttribute('download', name);
-    VigramButton.src = image;
-    VigramLink.appendChild(VigramButton);
-    VigramLink.appendChild(VigramText);
-    container.appendChild(VigramLink);
-    return container;
-
-}
-
-function getMeta(content)
-{
-    var pattern = '<video src="',
-        start = content.indexOf(pattern, 0) + pattern.length,
-        end = content.indexOf('>', start, 200) - 1,
-        url = content.substring(start, end).split('?')[0],
-        tmp = url.split('/'),
-        name = tmp[tmp.length - 1];
-    return {name:name, url:url};
-
-}
-
-function timelineVine(getVigramButton, classToClone, classToAppend, element)
-{
-    var linkSingleVine = element.querySelector('.share-overlay a');
-    if (linkSingleVine && linkSingleVine.href !== null)
-    {
-        ajax('GET', linkSingleVine.href, function(content) {
-            var vineMeta = getMeta(content),
-                VigramContainer = element.nextElementSibling.querySelector(classToClone).cloneNode(false);
-            element.nextElementSibling.querySelector(classToAppend).appendChild(getVigramButton(VigramContainer, vineMeta.url, vineMeta.name));
-        }, null);
-    }
-
-}
-
-function    mousemover(e) {
-    e = e ? e : window.event;
-
-    if (document.URL.split('/')[3] === 'v')
-    {
-        singleVine();
-    }
-    else if (e.target.tagName === "VIDEO")
-    {
-        var element = e.target.parentNode.parentNode.parentNode;
-        if (hasClass(element, "video-container") && !hasClass(element, 'Vigram'))
-        {
-            element.className += " Vigram";
-            timelineVine(getChannelsVigramButton, '.share', '.post-actions', element);
-        }
-    }
-};
-
-// console.log(window.location.origin)
-// document.addEventListener('mousemove', mousemover);
-
 var VigramVine = (function() {
+
+  function            getVigramButton(node)
+  {
+    var video = node.querySelector('video');
+    if (!video) {
+      return;
+    }
+    var metas = {
+        url: video.getAttribute('poster')
+    };
+    if (!metas.url || metas.url == 'default.mp4') {
+      return;
+    }
+
+    metas.name = metas.url.split('/')[4];
+
+    var link = document.createElement('a'),
+        button = document.createElement('button'),
+        span = document.createElement('span'),
+        caption = document.createElement('span');
+
+    button.className = "ember-view action clear share";
+    span.style.background = 'url(' + VigramLogo + ') no-repeat 50% 50%';
+    span.style.backgroundSize = '20px';
+    span.style.display = "block";
+    span.style.height = "20px";
+    span.style.width = "20px";
+    link.href = metas.url;
+    link.setAttribute('download', metas.name);
+    caption.className = 'caption';
+    caption.innerText = 'Vigram';
+
+    link.appendChild(span);
+    link.appendChild(caption);
+    button.appendChild(link);
+
+    return button;
+  }
 
   function            isMediaBlock(node)
   {
       return !!node &&
+              node.tagName == "DIV" &&
               !!node.classList &&
-              !!node.classList.contains('card');
+              !!node.classList.contains('post');
   }
 
   function    setButton(video)
@@ -146,30 +62,23 @@ var VigramVine = (function() {
       return;
     }
 
+    var node = video;
+    while (!isMediaBlock(node) && !!node) {
+      node = node.parentNode;
+    }
+    if (!node || node.tagName != "DIV") {
+
+      return;
+    }
     var button = getVigramButton(video);
     if (!button) {
 
       return;
     }
 
-    var node = video;
-    console.log(node);
-    while (!isMediaBlock(node) && !!node) {
-      node = node.parentNode;
-    }
-    if (!node) {
-
-      return;
-    }
-
-    console.log(node);
-    var commentNode = node.querySelector('.post-engagement');
-    console.log(commentNode);
-    commentNode.querySelector('.post-actions.with-captions').appendChild(button);
-
-
-
     video.classList.add('Vigram');
+    var commentNode = node.querySelector('.post-engagement');
+    commentNode.querySelector('.post-actions.with-captions').appendChild(button);
   }
 
 
@@ -179,9 +88,9 @@ var VigramVine = (function() {
 
       window.addEventListener('DOMSubtreeModified', function(e) {
 
-          var medias = document.querySelectorAll('video');
+          var medias = document.querySelectorAll('.post');
           // console.log(medias);
-          for (var k = 0; k < medias.length && k < 50; k++) {
+          for (var k = 0; k < medias.length; k++) {
               setButton(medias[k]);
           }
       });
